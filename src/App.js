f7f03f09dbfd043689dc6000755393eaff86e253
Andranik <andtsatur1296@gmail.com>
2022-02-07 17:17:00 +0400

    deletion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <div>Azatyan hovhannes</div>
       <div>Andranik tsaturyan</div>
-      <button onClick={() => {}}>click me</button>
+      <button>click me</button>
     </div>
   );
 }
