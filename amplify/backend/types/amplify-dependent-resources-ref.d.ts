c39a4297d47b13ef3b4d2c25205ff2d9f278037c
Andranik <andtsatur1296@gmail.com>
2022-02-07 17:12:32 +0400

    back commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "images5b29aadc": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "images": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "api19ee9595": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <div>Azatyan hovhannes</div>
       <div>Andranik tsaturyan</div>
+      <button onClick={() => {}}>click me</button>
     </div>
   );
 }
