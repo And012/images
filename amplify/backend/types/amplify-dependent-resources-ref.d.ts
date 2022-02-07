22298796b7087925c521ec51e2a89aecdcde856b
Andranik <andtsatur1296@gmail.com>
2022-02-07 13:49:07 +0400

    add backend configuration with graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "images": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import awsconfig from "./aws-exports";
+import { Amplify, API } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { useEffect, useState } from "react";
+import { listTodos } from "./graphql/queries";
+Amplify.configure(awsconfig);
+
+async function getList(api, setState) {
+  const result = await api.graphql({ query: listTodos });
+  console.log(result);
+  setState(result);
+}
 function App() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    getList(API, setList);
+  }, []);
   return (
     <div>
       <div>Azatyan hovhannes</div>
       <div>Andranik tsaturyan</div>
+      <button
+        onClick={async () => {
+          const result = await API.graphql({
+            query: createTodo,
+            variables: {
+              input: {
+                name: "some to do",
+              },
+            },
+          });
+          console.log(result);
+        }}
+      >
+        some create todo button
+      </button>
     </div>
   );
 }
