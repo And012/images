0853d506675dfbd4414e604ad9681f9ffc1bc36f
Andranik <andtsatur1296@gmail.com>
2022-02-07 17:49:22 +0400

    lksajdjflkasdf

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,5 +6,22 @@ export type AmplifyDependentResourcesAttributes = {
             "Region": "string",
             "LambdaExecutionRole": "string"
         }
+    },
+    "auth": {
+        "images89324eaf": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s30f5d1f02": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
